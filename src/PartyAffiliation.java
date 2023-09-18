7a4b2f896b321b6b22af6a6ffb100e7074754196
Matthew <augermjr@gmail.com>
2023-09-18 13:16:08 -0400

    Input prompt text adjusted

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -3,7 +3,7 @@ public class PartyAffiliation {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter your party affiliation(D, R, I, Other): ");
+        System.out.println("Enter your party affiliation(D, R, I or Other): ");
         String party = in.nextLine();
         if (party.toUpperCase().equals("D")) {
             System.out.println("You get a Democratic Donkey!");
